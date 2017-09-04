738805d0cd4592348bc4db3fd3e219367725f557
Max Morales <mamorales10@miners.utep.edu>
2017-09-04 13:02:27 -0600

    test.c works

--- tokenizer/test.c
+++ tokenizer/test.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include "mytoc.h"
 
+/* Prints the tokens in the vector */
 void printTokens(char **vect){
 
   char **tempTok = vect;
@@ -13,7 +14,38 @@ void printTokens(char **vect){
   
 }
 
+/* Checks if input equals exit */
+char isExit(char *str){
+
+  char exit[] = "exit";
+  int i;
+
+  for(i = 0; str[i] == exit[i]; i++){
+    if(str[i] == '\0')
+      return 1;
+  }
+  
+  return 0;
+  
+}
+
+/* Receives string and replaces '\n' with 0 */
+void getString(char* s, int limit){
+
+  char *p = s;
+  char c;
+  fgets(s, limit, stdin);
+  while((c = *p) && (c != '\n'))
+    p++;
+  if(c == '\n')
+    *p = 0;
+  
+}
+
 int main(){
+
+  char buff[200];
+  char **vect;
 
   char *test;
   //test = "Hello my dog's name is Darwin.";
@@ -21,15 +53,33 @@ int main(){
   //test = "";
   //test = " ";
   //test = " HelloWorld";
-
-  char **vect = mytoc((char *)test, ' ');
-  printTokens(vect);
   
-  /*
+  vect = mytoc((char *)test, ' ');
+  printTokens(vect);
+  free(vect);
+  
+  
   if(write(1, "$ ", 2) != 2){
     write(2, "There was an error.\n", 20);
     return -1;
-    }*/
-      
+  }
+
+  getString(buff, 200);
+  
+  while(!isExit(buff)){
+  
+    vect = mytoc((char *)buff, ' ');
+    printTokens(vect);
+    free(vect);
+
+    if(write(1, "$ ", 2) != 2){
+      write(2, "There was an error.\n", 20);
+      return -1;
+    }
+    getString(buff, 200);
+  
+  }
+  
+  printf("Program Ended\n");    
 
 }
